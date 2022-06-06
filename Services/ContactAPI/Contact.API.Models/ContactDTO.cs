da4586229544ae17b5c74ff222fa863fad9f7e5b
Emre Ozkan <ozkanyunusemre@outlook.com>
2022-06-06 17:19:40 +0300

    Add project files.

---
+++ Services/ContactAPI/Contact.API.Models/ContactDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contact.API.Models
+{
+    public class ContactDTO
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String FullName => $"{FirstName} {LastName}"; 
+    }
+}
---
+++ Services/RestAPI/Reservation.API/Controllers/ResController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResController : ControllerBase
+    {
+        private IReservationService reservationService;
+        public ResController(IReservationService IReservationService)
+        {
+            reservationService = IReservationService;
+        }
+        [HttpGet("{Id}")]
+        public ReservationDTO Get(int Id)
+        {
+           return reservationService.GetResByBkgNumber(Id);
+        }
+    }
+}
